9970aa2f17d816d697ef1c0a04183caf282da9c4
Luis <luisleo52655@gmail.com>
2023-10-26 01:04:09 +0800

    debug

--- install_paraview.sh
+++ install_paraview.sh
@@ -1,11 +1,11 @@
-#wget 'https://www.dropbox.com/scl/fi/5pc5ez93owfoh643bo9gk/ParaView-5.11.2-egl-MPI-Linux-Python3.9-x86_64.tar.gz?rlkey=k10isaw37phe1xl58zyetckv2' -O $HOME/paraview.tar.gz
+#wget 'https://www.dropbox.com/scl/fi/d288giczfxf6dpqfvavzo/paraview.tar.gz?rlkey=b2l5lfkml75twp58qsby80epf&dl=0' -O $HOME/paraview.tar.gz
 
-mkdir -p $HOME/ParaView-5.11.2-egl
+mkdir -p $HOME/ParaView-5.11.2
 
-tar zxvf $HOME/paraview.tar.gz -C $HOME/ParaView-5.11.2-egl
+tar zxvf $HOME/paraview.tar.gz -C $HOME/ParaView-5.11.2
 
-mv $HOME/ParaView-5.11.2-egl/ParaView-5.11.2-egl-MPI-Linux-Python3.9-x86_64/* $HOME/ParaView-5.11.2-egl
-rmdir $HOME/ParaView-5.11.2-egl/ParaView-5.11.2-egl-MPI-Linux-Python3.9-x86_64
+mv $HOME/ParaView-5.11.2/ParaView-5.11.2-osmesa-MPI-Linux-Python3.9-x86_64/* $HOME/ParaView-5.11.2
+rmdir $HOME/ParaView-5.11.2/ParaView-5.11.2-osmesa-MPI-Linux-Python3.9-x86_64
 
-echo 'export PATH=$HOME/ParaView-5.11.2-egl/bin:$PATH' >> $HOME/.bashrc
+echo 'export PATH=$HOME/ParaView-5.11.2/bin:$PATH' >> $HOME/.bashrc
 
